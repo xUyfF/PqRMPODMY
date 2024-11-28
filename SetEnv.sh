490c7b35d11180012ad25384548b11af10707a88
xUyfF <112759642+xUyfF@users.noreply.github.com>
2024-11-29 01:11:31 +0200

    解决了 空指针异常 Bug

---
+++ SetEnv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Shell script for configuring environment variables for SigLib DSP Library
+
+# SigLib Environment Variables
+export PATH=$PATH:$SIGLIB_PATH/utils:$SIGLIB_PATH/Examples/FileIO
+
+# GCC SigLib Environment Variables
+export C_INCLUDE_PATH=$C_INCLUDE_PATH:$SIGLIB_PATH/include:$SIGLIB_PATH/gnuplot_c/src
+export LIBRARY_PATH=$LIBRARY_PATH:$SIGLIB_PATH/lib/linux:$SIGLIB_PATH/gnuplot_c/src
